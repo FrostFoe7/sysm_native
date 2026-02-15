b2cc68770307de85ac87a4e0f2cc7bd4aca72884
FrostFoe <131578101+FrostFoe7@users.noreply.github.com>
2026-02-15 10:12:03 +0000

    lint safe DM encrypt fixed

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,21 @@ module.exports = function (api) {
   return {
     presets: [['babel-preset-expo'], 'nativewind/babel'],
 
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: [['babel-preset-expo'], 'nativewind/babel'],
+
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: [['babel-preset-expo'], 'nativewind/babel'],
+
     plugins: [
+      // transform import.meta for web compatibility
+      ['babel-plugin-transform-import-meta'],
       [
         'module-resolver',
         {
